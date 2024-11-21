183eb3e47571ad7bd8c85b1c819ad15c5d5972de
Alhamdanaaa <aalhamdana@gmail.com>
2024-11-21 16:31:59 +0700

    W12: Jawaban Soal 2

---
+++ pertemuan_12/stream_alhamdana/lib/stream.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ColorStrean {
+  final List<Color> colors = [
+    Colors.blueGrey,
+    Colors.amber,
+    Colors.deepPurple,
+    Colors.lightBlue,
+    Colors.teal,
+    Colors.black,
+    Colors.amberAccent,
+    Colors.deepOrange,
+    Colors.redAccent,
+    Colors.lightGreen
+  ];
+}
