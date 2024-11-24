f582329d55c4a01d596edc7e9fda3aec5a65fab2
Alhamdanaaa <aalhamdana@gmail.com>
2024-11-24 14:05:34 +0700

    W12: Jawaban Soal 6

--- pertemuan_12/stream_alhamdana/lib/stream.dart
+++ pertemuan_12/stream_alhamdana/lib/stream.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class ColorStream {
   Stream<Color> getColors() async* {
@@ -20,4 +21,14 @@ class ColorStream {
     Colors.redAccent,
     Colors.lightGreen
   ];
+}
+
+class NumberStream {
+  final StreamController<int> controller = StreamController<int>();
+  void addNumberToSink(int newNumber){
+    controller.sink.add(newNumber);
+  }
+  close(){
+    controller.close();
+  }
 }
