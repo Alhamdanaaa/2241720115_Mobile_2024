015de0a78684a69fc31e5b4e81419dbb2c0b981e
Alhamdanaaa <aalhamdana@gmail.com>
2024-11-24 14:31:35 +0700

    W12: Jawaban Soal 8

--- pertemuan_12/stream_alhamdana/lib/main.dart
+++ pertemuan_12/stream_alhamdana/lib/main.dart
@@ -31,6 +31,8 @@ class StreamHomePage extends StatefulWidget {
 }
 
 class _StreamHomePageState extends State<StreamHomePage> {
+  late StreamTransformer transformer;
+
   int lastNumber = 0;
   late StreamController numberStreamController;
   late NumberStream numberStream;
@@ -77,10 +79,29 @@ class _StreamHomePageState extends State<StreamHomePage> {
 
   @override
   void initState() {
+    transformer = StreamTransformer<int,int>.fromHandlers(
+      handleData: (value, sink) {
+        sink.add(value * 10);
+      },
+      handleError: (error, trace, sink) {
+        sink.add(-1);
+      },
+      handleDone: (sink) => sink.close()
+    );
+    
     numberStream = NumberStream();
     numberStreamController = numberStream.controller;
     Stream stream = numberStreamController.stream;
-    stream.listen((event) {
+    // stream.listen((event) {
+    //   setState(() {
+    //     lastNumber = event;
+    //   });
+    // }).onError((error) {
+    //   setState(() {
+    //     lastNumber = -1;
+    //   });
+    // });
+    stream.transform(transformer).listen((event) {
       setState(() {
         lastNumber = event;
       });
@@ -100,8 +121,8 @@ class _StreamHomePageState extends State<StreamHomePage> {
 
   void addRandomNumber() {
     Random random = Random();
-    // int myNum = random.nextInt(10);
-    // numberStream.addNumberToSink(myNum);
-    numberStream.addError();
+    int myNum = random.nextInt(10);
+    numberStream.addNumberToSink(myNum);
+    // numberStream.addError();
   }
 }
